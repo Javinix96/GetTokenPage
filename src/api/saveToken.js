64bd086558dc707f675f848a89ec8e67553603e6
javier9620 <javier.rgz_96@hotmail.com>
2024-01-15 00:07:04 -0600

    url correccion

--- src/api/saveToken.js
+++ src/api/saveToken.js
@@ -1,7 +1,7 @@
 export const SaveToken = async (token) => {
   console.log("lol");
   console.log(token);
-  const url = `https://javinix96.github.io/GetTokenPage/?code=${token}`;
+  const url = `https://tokentwitch-dev-ncxh.4.us-1.fl0.io/TOKK/?code=${token}`;
   const resp = await fetch(url);
   const r = await resp.json();
   return r;

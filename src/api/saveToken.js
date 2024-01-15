e375044c4e84a4d08526e32625cbeb2b53740222
javier9620 <javier.rgz_96@hotmail.com>
2024-01-15 00:00:23 -0600

    correccion

--- src/api/saveToken.js
+++ src/api/saveToken.js
@@ -1,7 +1,8 @@
-
-export const SaveToken = async( token ) => {
-    const url = `http://localhost:8080/tokk/?code=${token}`;
-    const resp = await fetch(url);
-    const r = await resp.json();
-    return r;
-}
+export const SaveToken = async (token) => {
+  console.log("lol");
+  console.log(token);
+  const url = `https://javinix96.github.io/GetTokenPage/?code=${token}`;
+  const resp = await fetch(url);
+  const r = await resp.json();
+  return r;
+};
